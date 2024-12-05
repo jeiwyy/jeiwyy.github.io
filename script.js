023b503b0f80de593e7538a30409df266ec3666f
jeiwyy <jeiwy1333@gmail.com>
2024-12-05 10:14:51 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function submitForm() {
+    const name = document.getElementById('name').value;
+    const email = document.getElementById('email').value;
+    alert(`Спасибо, ${name}. Ваша заявка отправлена.`);
+}
